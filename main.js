469c7ac45d8e72b59cf3453a6585a65c32f0801c
Dongjie <dongjielu@wustl.edu>
2020-09-07 21:44:24 +0800

    init

---
+++ main.js
@@ -0,0 +1,67 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2020", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+const galleonCoin = new Blockchain();
+galleonCoin.addBlock(new Block(1, "06/09/2020", { amount: 6 }));
+galleonCoin.addBlock(new Block(2, "07/09/2020", { amount: 7 }));
+
+galleonCoin.chain[2].data = { amount: 100 };
+galleonCoin.chain[2].hash = galleonCoin.chain[2].calculateHash();
+
+console.log(galleonCoin.isChainValid());
+// console.log(JSON.stringify(galleonCoin, null, 4));
